8e75315196bfe4b3e016c6c89f8f1c17a658635e
BruceLuo <295448382@qq.com>
2019-09-27 00:16:37 +0800

    '这里把按钮轮播图面板抽象出来分别单独作为一个组件来使用'

--- app/js/core/panel.vue
+++ app/js/core/panel.vue
@@ -1,15 +1,26 @@
 <template>
-  <section class="panel">
-      <h4></h4>
-      <div></div>
+  <section :class="[panelClass,cname]">
+      <h4>-{{title}}-</h4>
+      <!-- <div>{{content}}</div> -->
+      <slot/>
   </section>
 </template>
 
 <script>
 export default {
+    props:{
+        cname:{
+            type: String,
+            default: ""
+        },
+        title:{
+            type: String,
+            default: ""
+        }
+    },
     data() {
         return {
-
+            panelClass: 'panel'
         }
     }
 }
---
+++ app/js/core/slider.vue
@@ -0,0 +1,46 @@
+<template>
+    <section :class="cname">
+        <swiper :options="options" :not-next-tick="options.notNextTick">
+            <swiper-slide v-for="item in items" :key="item.href">
+                <router-link to="item.href">
+                     <img :src="item.src" alt="this is a picture">
+                </router-link>
+            </swiper-slide>
+        </swiper>
+        <div class="swiper-pagination" v-if="options.pagenation"></div>
+    </section>
+</template>
+
+<script>
+import { swiper, swiperSlide} from "vue-awesome-swiper"
+export default {
+    components:{
+        swiper,
+        swiperSlide
+    },
+    props:{
+        options:{
+            type:Object,
+            default() {
+                return {
+                    autoplay: true,
+                    loop: true,
+                    pagination: {
+                        el:".swiper-pagination"
+                    },
+                    notNextTick: false
+                }
+            }
+        },
+        items:{
+            type: Array,
+            default() {
+                return []
+            }
+        }
+    }
+}
+</script>
+<style lang='scss'>
+    @import "~swiper/dist/css/swiper.css";
+</style>
